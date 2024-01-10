c1e0adb4b0b0f063bd8119fa498ff2380c745616
sandeep3468 <s.sandeep2197@gmail.com>
2024-01-10 20:53:24 +0530

    express-node-server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
