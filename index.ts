94bfbea37a53199198e25b6a8216d1916fa36475
Sandeep Singh <sandeepitgcet@gmail.com>
2024-05-12 16:20:24 +0530

    updated

--- index.ts
+++ index.ts
@@ -13,8 +13,4 @@ app.get("/home", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello Home World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-
 export default app;
